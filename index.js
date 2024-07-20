1818cbe79f3c416ce1bcea26a32ac4a36014e8b7
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-21 00:42:50 +0700

    up4

--- index.js
+++ index.js
@@ -9,8 +9,8 @@ app.use(express.json());
 const postRouter = require("./routes/Posts");
 app.use("/posts", postRouter);
 
-// db.sequelize.sync().then(() => {
-app.listen(3001, () => {
-  console.log("server running");
+db.sequelize.sync().then(() => {
+  app.listen(3001, () => {
+    console.log("server running");
+  });
 });
-// });
