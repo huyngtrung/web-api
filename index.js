0a194811d539f7fa4ae7b39ca33b92acfed37ad2
huyngtrung <164539209+huyngtrung@users.noreply.github.com>
2024-07-23 21:38:25 +0700

    updatecors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
+const cors = require("cors");
 
 const db = require("./models");
 
 app.use(express.json());
+app.use(cors());
 
 // Routers
 const postRouter = require("./routes/Posts");
